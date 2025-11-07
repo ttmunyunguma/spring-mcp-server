bca476f547dac41d44e82fdfeb6c567b703e6050
Terrence T Munyunguma <terrence.munyunguma@ibm.com>
2025-11-07 20:07:33 +0000

    init

---
+++ src/main/java/com/cuius/mcpserver/SpringMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.cuius.mcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMcpServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cuius/mcpserver/SpringMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuius.mcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
