28f5846cdf2de36197dcff2466c991fd7394541e
Terrence T Munyunguma <terrence.munyunguma@ibm.com>
2025-11-10 23:04:08 +0000

    mcp server

--- src/main/java/com/cuius/mcpserver/SpringMcpServerApplication.java
+++ src/main/java/com/cuius/mcpserver/SpringMcpServerApplication.java
@@ -1,13 +1,23 @@
 package com.cuius.mcpserver;
 
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class SpringMcpServerApplication {
+    public static final Logger logger = Logger.getLogger(SpringMcpServerApplication.class.getName());
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMcpServerApplication.class, args);
     }
 
+    @Bean
+    public ToolCallbackProvider weatherTools(WeatherService weatherService){
+        return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
+    }
 }
